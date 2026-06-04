f689fa07e4f559a35bb753c6f966e3a227735604
ValenAbarzua <valefer119@gmail.com>
2026-06-04 17:36:56 -0300

    Estructura MVC

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import mongoose from 'mongoose';
+import mongooseConfig from './src/config/mongoConfig.js';
+
+const app = express();
+const PORT = process.env.PORT;
+
+const server = http.createServer();
+
+app.use(cors());
+app.use(express.json());
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
