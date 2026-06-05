efa6af1cb2210dd047a105f639bbf9f4f695f9fe
ValenAbarzua <valefer119@gmail.com>
2026-06-05 11:36:26 -0300

    Conexion a MongoDB

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import dotenv from 'dotenv';
-import mongooseConfig from './src/config/mongoConfig.js';
+import connectDB from './src/config/mongoConfig.js';
 
+dotenv.config();
+connectDB();
 const app = express();
 const PORT = process.env.PORT;
-
-const server = http.createServer();
 
 app.use(cors());
 app.use(express.json());
 
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
